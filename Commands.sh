f01654cb35608e66df216affc227ef97eac2fef1
jose.alvarez <jose.alvarez@tata.com.uy>
2023-05-10 17:10:21 +0200

    borrar y crear backup de folder

--- Commands.sh
+++ Commands.sh
@@ -4,3 +4,8 @@ cat "crontab_log.txt" | grep "(username)" | grep -o "CMD.*" | sort | uniq
 # Find files (logs) with a name
 grep -Ril "name"
 
+# delete folder and subfolders
+rm -rf folder_name
+
+# copy folder and subfolder (backup)
+cp -r -u <folder_name> <folder_name_backup>`date +"%Y%m%d"`
